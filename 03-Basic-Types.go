19f9cff386cbaf539fa7a714963f0f1562cb5614
Vitor Vieira <vitorsvieira@yahoo.com>
2014-08-05 14:54:17 -0300

    first commit

---
+++ 03-Basic-Types.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	//bool
+
+	//string
+
+	//int
+
+	//int8
+
+	//int16
+
+	//int32
+
+	//int64
+
+	//uint
+
+	//uint8
+
+	//uint16
+
+	//uint32
+
+	//uint64
+
+	//uintptr
+
+	//byte(uint8)
+
+	//rune(int8), similar to char
+
+	//float32
+
+	//float64
+
+	//complex64
+
+	//complex128
+}

593738a549f97453e361bb72ce6f4fb693e1d8f8
Vitor Vieira <vitorsvieira@yahoo.com>
2014-08-06 01:16:13 -0300

    .

--- 13-Passing-A-Function-As-Parameter.go
+++ 13-Passing-A-Function-As-Parameter.go
@@ -9,7 +9,14 @@ type Plate struct {
 	vegetable string
 }
 
+type JustCook func(string)
+
 func CookLunch(lunch Plate, do func(string)) {
+	course := lunch.pasta + " with " + lunch.vegetable
+	do(course)
+}
+
+func CookDinner(lunch Plate, do JustCook) {
 	course := lunch.pasta + " with " + lunch.vegetable
 	do(course)
 }
@@ -22,5 +29,5 @@ func main() {
 	lunch := Plate{"Fettuccine", "Pumpkin"}
 
 	CookLunch(lunch, SimpleMenu)
-
+	CookDinner(lunch, SimpleMenu)
 }

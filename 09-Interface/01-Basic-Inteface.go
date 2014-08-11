8c7ff0ad50f7329144db0b4b02beb1ac20a9bf47
Vitor Vieira <vitorsvieira@yahoo.com>
2014-08-11 19:04:24 -0300

    .

--- 09-Interface/01-Basic-Inteface.go
+++ 09-Interface/01-Basic-Inteface.go
@@ -8,6 +8,10 @@ type Student struct {
 	FirstName, LastName string
 }
 
+type Instructor struct {
+	FirstName, Course string
+}
+
 type FullNamer interface {
 	Fullname() string
 }
@@ -17,14 +21,21 @@ func (student *Student) Fullname() string {
 	return fmt.Sprintf("%s %s", student.FirstName, student.LastName)
 }
 
-func CallStudent(f FullNamer) string {
+func (instructor *Instructor) Fullname() string {
+
+	return fmt.Sprintf("%s from %s course", instructor.FirstName, instructor.Course)
+}
+
+func Welcome(f FullNamer) string {
 
 	return fmt.Sprintf("Welcome! %s", f.Fullname())
 }
 
 func main() {
 
-	student := &Student{"Rob", "Pike"}
+	student := &Student{"John", "Doe"}
+	instructor := &Instructor{"Rob", "Golang"}
 
-	fmt.Println(CallStudent(student))
+	fmt.Println(Welcome(student))
+	fmt.Println(Welcome(instructor))
 }

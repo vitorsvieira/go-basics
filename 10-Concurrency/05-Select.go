d39cb07caa6ba9abc0be1091333da742612bf73d
Vitor Vieira <vitorsvieira@yahoo.com>
2014-08-12 02:36:52 -0300

    .

--- 10-Concurrency/05-Select.go
+++ 10-Concurrency/05-Select.go
@@ -10,13 +10,29 @@ type Numbers []Number
 func main() {
 
 	evenNumbers := EvenNumbers(50)
+	oddNumbers := OddNumbers(50)
 
-	c := make(chan Number)
+	chan1 := make(chan Number)
+	chan2 := make(chan Number)
 
-	go evenNumbers.ChannelNumbers(c)
+	go evenNumbers.ChannelNumbers(chan1)
+	go oddNumbers.ChannelNumbers(chan2)
 
-	for n := range c {
-		fmt.Println(n)
+	for {
+		select {
+		case n1, ready1 := <-chan1:
+			if ready1 {
+				fmt.Println("Chan1:", n1)
+			} else {
+				return
+			}
+		case n2, ready2 := <-chan2:
+			if ready2 {
+				fmt.Println("Chan2:", n2)
+			} else {
+				return
+			}
+		}
 	}
 }
 
@@ -33,6 +49,19 @@ func EvenNumbers(stop Number) *Numbers {
 	numbers := &Numbers{}
 
 	var i Number = 2
+
+	for i <= stop {
+		*numbers = append(*numbers, i)
+		i = i + 2
+	}
+	return numbers
+}
+
+func OddNumbers(stop Number) *Numbers {
+
+	numbers := &Numbers{}
+
+	var i Number = 1
 
 	for i <= stop {
 		*numbers = append(*numbers, i)
